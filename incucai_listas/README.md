eb22cddcae67a85105a97978f0f280d9fbfea1b6
Francisco Scagliarini <fscagliarini@incucai.gov.ar>
2025-09-03 15:29:29 -0300

    listas

---
+++ incucai_listas/README.md
@@ -0,0 +1,3 @@
+# incucai_listas
+
+A new Flutter project.
---
+++ incucai_listas/android/app/src/main/kotlin/com/example/incucai_listas/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.incucai_listas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
